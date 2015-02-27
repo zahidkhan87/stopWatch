85df5c857db7aacc2c6db9494b01938f3ca11ad3
cakers5559 <cakers5559@gmail.com>
2015-02-27 14:29:11 -0500

    added a formating method to format timer count properly

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,10 +1,19 @@
 // StopWatch Example Pure JS
-function stopWatch() {
-	var startText = document.querySelector('#timer').innerHTML,
-		stopText,
-		timer,
-		reset,
-		t = [0, 0, 0, 0, 0, 0, 0, 0];
+function stopWatch() {			
+	var timer = document.querySelector('#timer');		
+	var t = [0, 0, 0, 0, 0, 0, 0, 0];
+			
+	format = function(ms) {		
+		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+		var x = String(ms % 1000);
+		while (x.length < 3) {
+			x = '0' + x;
+		}
+		d += '.' + x;
+		return d.substr(0, d.length - 1);
+	};
+	
 	
 }
+
 
