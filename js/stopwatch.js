15c0d52d326d1a6fef1bcb7b1cbf2351cadaf270
cakers5559 <cakers5559@gmail.com>
2015-02-27 14:47:00 -0500

    added start and stop funtion to count. Also updated the css to look more like iphone

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,8 +1,9 @@
 // StopWatch Example Pure JS
 function stopWatch() {			
-	var timer = document.querySelector('#timer');		
-	var t = [0, 0, 0, 0, 0, 0, 0, 0];
-			
+	var timer = document.querySelector('.sec-big');
+	var timerSm = document.querySelector('.sec-small');
+	var t = [0, 0, 0, 0, 0, 0, 0, 0];		
+	
 	format = function(ms) {		
 		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
 		var x = String(ms % 1000);
@@ -11,9 +12,40 @@ function stopWatch() {
 		}
 		d += '.' + x;
 		return d.substr(0, d.length - 1);
-	};
+	};	
 	
+	this.startStop = function() {				
+		t[t[2]] = (+new Date()).valueOf();
+		t[2] = 1 - t[2];
+		
+		if (t[2] == 0) {
+			clearInterval(t[4]);
+			t[3] += t[1] - t[0];
+			
+			
+			t[7]++;
+			
+			t[4] = t[1] = t[0] = 0;
+			
+			display();
+		}
+		else {
+			
+			t[4] = setInterval(display, 40);
+		}
+		
+		return false;
+	};	
 	
+	display = function() {		
+		if (t[2]) {
+			t[1] = (new Date()).valueOf();
+		}
+		
+		timer.innerHTML = format(t[3] + t[1] - t[0]);
+		timerSm.innerHTML = format(t[3] + t[1] - t[0]);
+	};					
 }
+
 
 
