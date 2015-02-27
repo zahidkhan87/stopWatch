eecaeff73c00fd97c9539a0b9a1273b47bbab0ef
cakers5559 <cakers5559@gmail.com>
2015-02-27 14:16:22 -0500

    Set some namespaces in stopwatch.js

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,12 +1,10 @@
-var stopWatch = {
-	start : function() {
-		
-		
-		
-	},
+// StopWatch Example Pure JS
+function stopWatch() {
+	var startText = document.querySelector('#timer').innerHTML,
+		stopText,
+		timer,
+		reset,
+		t = [0, 0, 0, 0, 0, 0, 0, 0];
 	
-	reset : function() {
-		
-		
-	}			
-};
+}
+
