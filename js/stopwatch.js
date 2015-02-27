35e8e837d1f9e333ec4a6a0161a38a54f1c2c20b
cakers5559 <cakers5559@gmail.com>
2015-02-27 14:56:26 -0500

    Start is currently working on app load. Need to connect the startStop button to it

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -5,15 +5,15 @@ function stopWatch() {
 	var t = [0, 0, 0, 0, 0, 0, 0, 0];		
 	
 	format = function(ms) {		
-		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}).*/, '$1');
 		var x = String(ms % 1000);
 		while (x.length < 3) {
 			x = '0' + x;
 		}
 		d += '.' + x;
 		return d.substr(0, d.length - 1);
-	};	
-	
+	};
+			
 	this.startStop = function() {				
 		t[t[2]] = (+new Date()).valueOf();
 		t[2] = 1 - t[2];
@@ -21,16 +21,14 @@ function stopWatch() {
 		if (t[2] == 0) {
 			clearInterval(t[4]);
 			t[3] += t[1] - t[0];
-			
-			
+						
 			t[7]++;
 			
 			t[4] = t[1] = t[0] = 0;
 			
 			display();
 		}
-		else {
-			
+		else {			
 			t[4] = setInterval(display, 40);
 		}
 		
@@ -47,5 +45,6 @@ function stopWatch() {
 	};					
 }
 
-
+var start = new stopWatch();
+start.startStop();
 
