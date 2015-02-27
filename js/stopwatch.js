cab1adaf0aa804d6324a0619573a4f523d562867
cakers5559 <cakers5559@gmail.com>
2015-02-27 15:05:36 -0500

    Start and stop with color changes. Also reset functionality is working

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -4,16 +4,6 @@ function stopWatch() {
 	var timerSm = document.querySelector('.sec-small');
 	var t = [0, 0, 0, 0, 0, 0, 0, 0];		
 	
-	format = function(ms) {		
-		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}).*/, '$1');
-		var x = String(ms % 1000);
-		while (x.length < 3) {
-			x = '0' + x;
-		}
-		d += '.' + x;
-		return d.substr(0, d.length - 1);
-	};
-			
 	this.startStop = function() {				
 		t[t[2]] = (+new Date()).valueOf();
 		t[2] = 1 - t[2];
@@ -26,16 +16,47 @@ function stopWatch() {
 			
 			t[4] = t[1] = t[0] = 0;
 			
+			document.querySelector('#startTimer').innerHTML = "Start";
+			document.querySelector('#startTimer').style.color = "green";
+			document.querySelector('#reset').style.display = "block";
+			
 			display();
 		}
 		else {			
+			document.querySelector('#startTimer').innerHTML = "Stop";
+			document.querySelector('#startTimer').style.color = "red";
+			document.querySelector('#reset').style.display = "none";
 			t[4] = setInterval(display, 40);
 		}
 		
 		return false;
 	};	
 	
-	display = function() {		
+	this.reset = function() {
+		if (t[2]) {
+			startStop();
+		}
+		
+		t[4] = t[3] = t[2] = t[1] = t[0] = 0;
+		
+		display();
+		
+		t[7] = 0;
+		
+		return false;
+	};
+	
+	var format = function(ms) {		
+		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}).*/, '$1');
+		var x = String(ms % 1000);
+		while (x.length < 3) {
+			x = '0' + x;
+		}
+		d += '.' + x;
+		return d.substr(0, d.length - 1);
+	};		
+	
+	var display = function() {		
 		if (t[2]) {
 			t[1] = (new Date()).valueOf();
 		}
@@ -46,5 +67,5 @@ function stopWatch() {
 }
 
 var start = new stopWatch();
-start.startStop();
+
 
