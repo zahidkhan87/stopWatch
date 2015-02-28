f0bf60f083e04f3a611fe5c5ce4bf68eb6d5a9cb
cakers5559 <cakers5559@gmail.com>
2015-02-27 22:01:23 -0500

    Added Lap functionality. Made design more similiar to that of the iphone app.

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,10 +1,10 @@
-
 // StopWatch Example Pure JS
 function stopWatch() {	
 	// set some vars
 	var timer = document.querySelector('.sec-big');
 	var timerSm = document.querySelector('.sec-small');
-	var t = [0, 0, 0, 0, 0, 0, 0, 0];		
+	var t = [0, 0, 0, 0, 0, 0, 0, 0];
+	var laps = 0;
 	
 	// toggle start and stop function
 	this.startStop = function() {				
@@ -21,13 +21,13 @@ function stopWatch() {
 			
 			document.querySelector('#startTimer').innerHTML = "Start";
 			document.querySelector('#startTimer').style.color = "green";
-			document.querySelector('#reset').style.display = "block";			
+			document.querySelector('#reset').innerHTML = "Reset";
 			display();
 		}
 		else {			
-			document.querySelector('#startTimer').innerHTML = "Stop";
-			document.querySelector('#startTimer').style.color = "red";
-			document.querySelector('#reset').style.display = "none";
+			document.querySelector('#startTimer').innerHTML = "Stop";			
+			document.querySelector('#startTimer').style.color = "red";			
+			document.querySelector('#reset').innerHTML = "Lap";
 			t[4] = setInterval(display, 40);
 		}
 		
@@ -37,14 +37,23 @@ function stopWatch() {
 	// reset the timer
 	this.reset = function() {
 		if (t[2]) {
-			startStop();
-		}
+			laps++;
+			
+			document.querySelector('.time-lapsed ul').
+			insertBefore(document.createElement('li'), document.querySelector('.time-lapsed ul').firstChild);
+			
+			var firstChild = document.querySelector('.time-lapsed ul').firstChild;
+			firstChild.innerHTML = '<div>Lap '+laps+'</div><div>'+document.querySelector('.sec-big').textContent+'</div>';			
+		}		
+		else {
+			t[4] = t[3] = t[2] = t[1] = t[0] = 0;
 		
-		t[4] = t[3] = t[2] = t[1] = t[0] = 0;
+			display();
 		
-		display();
-		
-		t[7] = 0;
+			t[7] = 0;
+			document.querySelector('.time-lapsed ul').innerHTML = "";
+			laps = 0;
+		}				
 		
 		return false;
 	};
